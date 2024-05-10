b738c1f8820475cf3bd6bbca8da4b6bc430b0a17
Divine Ndaya Badibanga <divine@training-works.ca>
2024-05-10 00:05:05 -0230

    toggle active states on nav menu based on active sections

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from '../Media/favicon.ico';
 
 const Header = () => {
     const [isNavbarActive, setNavbarActive] = useState(false); // State to manage navbar visibility
+    const [activeSection, setActiveSection] = useState('');
+
+    const handleScroll = () => {
+      const sections = document.querySelectorAll('section');
+      let index = sections.length;
+
+      // Determine which section is currently in view
+      while (--index && window.scrollY + 300 < sections[index].offsetTop) {}
+
+      const activeId = sections[index]?.id || '';
+      setActiveSection(activeId);
+    };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    handleScroll(); // Initialize on first load
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
     const toggleNavbar = () => {
         setNavbarActive(!isNavbarActive); // Toggle the navbar visibility state
@@ -15,11 +36,11 @@ const Header = () => {
               Growing Tree Kindergarten
             </a>
             <nav className={`navbar ${isNavbarActive ? 'active' : ''}`}>
-                <a href="#about">About</a>
-                <a href='#services'>Services</a>
-                <a href="#education">Education</a>
-                <a href="#gallery">Gallery</a>
-                <a href="#contact">Contact</a>
+                <a href="#about" className={activeSection === 'about' ? 'active' : ''} >About</a>
+                <a href="#services" className={activeSection === 'services' ? 'active' : ''}>Services</a>
+                <a href="#education" className={activeSection === 'education' ? 'active' : ''}>Education</a>
+                <a href="#gallery" className={activeSection === 'gallery' ? 'active' : ''}>Gallery</a>
+                <a href="#contact" className={activeSection === 'contact' ? 'active' : ''}>Contact</a>
             </nav>
 
             <div className='icons'>
